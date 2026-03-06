8c885323457c729580c1e81608179192b34d85dd
repalleramr <repalleramr@gmail.com>
2026-03-06 11:56:48 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+
+const CACHE='bacc-v24-cache';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
